bbe51be9508d345d26ac7bfc91f853d304648d7a
Alexander <9726640@gmail.com>
2019-10-03 13:50:26 +0300

    Done

--- src/ru/avalon/java/ocpjp/labs/tasks/objects/CitizenSource.java
+++ src/ru/avalon/java/ocpjp/labs/tasks/objects/CitizenSource.java
@@ -11,25 +11,24 @@ import java.util.List;
 import java.util.Random;
 
 /**
- *
  * @author Constantine
  */
-public class CitizenSource implements Iterable<Citizen>{
+public class CitizenSource implements Iterable<Citizen> {
 
     private List<Citizen> citizen;
 
     public CitizenSource() {
-    Random random = new Random();
-    citizen = new ArrayList();
-    int count = random.nextInt(10)+20;
-    for(int i=0; i < count; i++) {
-        citizen.add(new RandomCitizen().create());
-	        }        
+        Random random = new Random();
+        citizen = new ArrayList<>();
+        int count = random.nextInt(10) + 20;
+        for (int i = 0; i < count; i++) {
+            citizen.add(new RandomCitizen().create());
+        }
     }
 
     @Override
     public Iterator<Citizen> iterator() {
         return citizen.iterator();
     }
-    
+
 }
